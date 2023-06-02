f79137343c5abc3d8fc87e0b23246346d3ecd3c7
Remtosh <110778018+Remtosh@users.noreply.github.com>
2023-06-02 07:46:31 +0300

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,23 @@
+<?php
+session_start(); 
+include("connection.php");
+include("functions.php");
+
+$user_data = check_sum($con);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>Welcome to Vibrant Internet</h1>
+        <br>
+        Hello, Username
+        <br>
+        <br>
+        <a href="logout.php">Logout</a>
+    </body>
+</html>
